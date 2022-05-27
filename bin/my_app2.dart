b980fb335cdfcd3d7e5c9d39f0a0ecc0864e3548
Omerun <1513finger@gmail.com>
2022-05-27 16:53:07 +0700

    update

---
+++ bin/my_app2.dart
@@ -0,0 +1,27 @@
+void main() {
+  //Declarative Variables
+  int? score = 50; // Local Varaible
+  double height = 165.5;
+  String hero ="Iron Man";
+  dynamic isTrue = true;
+  var message ="Welcome";
+  dynamic age = 25;
+  age = 100;
+  age = 50;
+  age = [10,20,30,40];
+
+  final String name;
+  const String country ="Thailand";
+  name = "Elon Mask";
+
+  //Display
+  print(country);
+  print(name);
+
+  print(age);
+  print(score + height);
+  print(hero);
+  print(isTrue);
+  print(message);
+
+}
